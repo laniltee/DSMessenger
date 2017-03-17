4d6e22ad3aba83dd858a604b41d9282a59a31750
laniltee <lanilmarasinghe@gmail.com>
2017-03-17 23:28:58 +0530

    Added support for sending unicast messages only for one client

--- src/ds_lab_1/ChatClient.java
+++ src/ds_lab_1/ChatClient.java
@@ -17,18 +17,9 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 /**
- * A simple Swing-based client for the chat server. Graphically it is a frame
- * with a text field for entering messages and a textarea to see the whole
- * dialog.
+ * A simple Swing-based client for the chat server. Graphically it is a frame with a text field for entering messages and a textarea to see the whole dialog.
  *
- * The client follows the Chat Protocol which is as follows. When the server
- * sends "SUBMITNAME" the client replies with the desired screen name. The
- * server will keep sending "SUBMITNAME" requests as long as the client submits
- * screen names that are already in use. When the server sends a line beginning
- * with "NAMEACCEPTED" the client is now allowed to start sending the server
- * arbitrary strings to be broadcast to all chatters connected to the server.
- * When the server sends a line beginning with "MESSAGE " then all characters
- * following this string should be displayed in its message area.
+ * The client follows the Chat Protocol which is as follows. When the server sends "SUBMITNAME" the client replies with the desired screen name. The server will keep sending "SUBMITNAME" requests as long as the client submits screen names that are already in use. When the server sends a line beginning with "NAMEACCEPTED" the client is now allowed to start sending the server arbitrary strings to be broadcast to all chatters connected to the server. When the server sends a line beginning with "MESSAGE " then all characters following this string should be displayed in its message area.
  */
 public class ChatClient {
 
@@ -39,36 +30,39 @@ public class ChatClient {
     JTextArea messageArea = new JTextArea(8, 40);
 
     private String clientName;
-    private String[] allClients = {"Cat", "Bat", "Rat"};
+    private String[] allClients = {"All"};
     private boolean isUnicast;
     JComboBox clientList = new JComboBox(allClients);
 
     /**
-     * Constructs the client by laying out the GUI and registering a listener
-     * with the textfield so that pressing Return in the listener sends the
-     * textfield contents to the server. Note however that the textfield is
-     * initially NOT editable, and only becomes editable AFTER the client
-     * receives the NAMEACCEPTED message from the server.
+     * Constructs the client by laying out the GUI and registering a listener with the textfield so that pressing Return in the listener sends the textfield contents to the server. Note however that the textfield is initially NOT editable, and only becomes editable AFTER the client receives the NAMEACCEPTED message from the server.
      */
     public ChatClient() {
 
         // Layout GUI
         textField.setEditable(false);
         messageArea.setEditable(false);
-        frame.getContentPane().add(textField, "North");
-        frame.getContentPane().add(clientList, "Center");
-        frame.getContentPane().add(new JScrollPane(messageArea), "South");
+        frame.getContentPane().add(textField, "South");
+        frame.getContentPane().add(clientList, "North");
+        frame.getContentPane().add(new JScrollPane(messageArea), "Center");
+        textField.requestFocus();
         frame.pack();
 
         // Add Listeners
         textField.addActionListener(new ActionListener() {
             /**
-             * Responds to pressing the enter key in the textfield by sending
-             * the contents of the text field to the server. Then clear the text
-             * area in preparation for the next message.
+             * Responds to pressing the enter key in the textfield by sending the contents of the text field to the server. Then clear the text area in preparation for the next message.
              */
             public void actionPerformed(ActionEvent e) {
-                out.println(textField.getText());
+                //Here we must check whether user has selected a recipent from combo boc
+                //If selected we must send the message to intended recipent only
+                String selectedClient = clientList.getSelectedItem().toString();
+                if (selectedClient.equals("All")) {
+                    out.println(textField.getText());
+                } else {
+                    isUnicast = true;
+                    out.println(selectedClient + ">>" + textField.getText());
+                }
                 textField.setText("");
             }
         });
@@ -121,10 +115,13 @@ public class ChatClient {
             } else if (line.startsWith("MESSAGE")) {
                 messageArea.append(line.substring(8) + "\n");
             } else if (line.startsWith("CLIENTLIST")) {
-                String mixString = line.substring(11);
+                String mixString = line.substring(10);
                 String clientArray[] = mixString.split(",");
                 clientList.removeAllItems();
                 for (int i = 0; i < clientArray.length; i++) {
+                    if (clientArray[i].equals(clientName)) {
+                        continue;
+                    }
                     clientList.addItem(clientArray[i]);
                 }
             }
--- src/ds_lab_1/ChatServer.java
+++ src/ds_lab_1/ChatServer.java
@@ -11,19 +11,11 @@ import java.util.HashSet;
 import java.util.Iterator;
 
 /**
- * A multithreaded chat room server. When a client connects the server requests
- * a screen name by sending the client the text "SUBMITNAME", and keeps
- * requesting a name until a unique one is received. After a client submits a
- * unique name, the server acknowledges with "NAMEACCEPTED". Then all messages
- * from that client will be broadcast to all other clients that have submitted a
- * unique screen name. The broadcast messages are prefixed with "MESSAGE ".
+ * A multithreaded chat room server. When a client connects the server requests a screen name by sending the client the text "SUBMITNAME", and keeps requesting a name until a unique one is received. After a client submits a unique name, the server acknowledges with "NAMEACCEPTED". Then all messages from that client will be broadcast to all other clients that have submitted a unique screen name. The broadcast messages are prefixed with "MESSAGE ".
  *
- * Because this is just a teaching example to illustrate a simple chat server,
- * there are a few features that have been left out. Two are very useful and
- * belong in production code:
+ * Because this is just a teaching example to illustrate a simple chat server, there are a few features that have been left out. Two are very useful and belong in production code:
  *
- * 1. The protocol should be enhanced so that the client can send clean
- * disconnect messages to the server.
+ * 1. The protocol should be enhanced so that the client can send clean disconnect messages to the server.
  *
  * 2. The server should do some logging.
  */
@@ -35,21 +27,18 @@ public class ChatServer {
     private static final int PORT = 9001;
 
     /**
-     * The set of all names of clients in the chat room. Maintained so that we
-     * can check that new clients are not registering name already in use.
+     * The set of all names of clients in the chat room. Maintained so that we can check that new clients are not registering name already in use.
      */
     private static HashSet<String> names = new HashSet<String>();
 
     /**
-     * The set of all the print writers for all the clients. This set is kept so
-     * we can easily broadcast messages.
+     * The set of all the print writers for all the clients. This set is kept so we can easily broadcast messages.
      */
     private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
     private static HashMap<String, PrintWriter> writerMap = new HashMap<String, PrintWriter>();
 
     /**
-     * The appplication main method, which just listens on a port and spawns
-     * handler threads.
+     * The appplication main method, which just listens on a port and spawns handler threads.
      */
     public static void main(String[] args) throws Exception {
         System.out.println("The chat server is running.");
@@ -64,30 +53,24 @@ public class ChatServer {
     }
 
     /**
-     * A handler thread class. Handlers are spawned from the listening loop and
-     * are responsible for a dealing with a single client and broadcasting its
-     * messages.
+     * A handler thread class. Handlers are spawned from the listening loop and are responsible for a dealing with a single client and broadcasting its messages.
      */
     private static class Handler extends Thread {
-        
+
         private String name;
         private Socket socket;
         private BufferedReader in;
         private PrintWriter out;
 
         /**
-         * Constructs a handler thread, squirreling away the socket. All the
-         * interesting work is done in the run method.
+         * Constructs a handler thread, squirreling away the socket. All the interesting work is done in the run method.
          */
         public Handler(Socket socket) {
             this.socket = socket;
         }
 
         /**
-         * Services this thread's client by repeatedly requesting a screen name
-         * until a unique one has been submitted, then acknowledges the name and
-         * registers the output stream for the client in a global set, then
-         * repeatedly gets inputs and broadcasts them.
+         * Services this thread's client by repeatedly requesting a screen name until a unique one has been submitted, then acknowledges the name and registers the output stream for the client in a global set, then repeatedly gets inputs and broadcasts them.
          */
         @Override
         public void run() {
@@ -113,9 +96,6 @@ public class ChatServer {
                             System.out.println("New Client Added: " + name);
                             names.add(name);
                             writerMap.put(name, out);
-                            
-                            out.println("CLIENTLIST" + getAllClients());
-                            
                             break;
                         }
                     }
@@ -126,6 +106,9 @@ public class ChatServer {
                 // this client can receive broadcast messages.
                 out.println("NAMEACCEPTED");
                 writers.add(out);
+
+                //Now we must populate the ComboBoxes in client environments with the name of all the clients
+                updateClients();
                 
                 // Accept messages from this client and broadcast them.
                 // Ignore other clients that cannot be broadcasted to.
@@ -144,18 +127,20 @@ public class ChatServer {
                     if (input == null) {
                         return;
                     }
-                    
+
                 }
             } catch (IOException e) {
                 System.out.println(e);
             } finally {
                 // This client is going down!  Remove its name and its print
                 // writer from the sets, and close its socket.
+                writerMap.remove(name);
                 if (name != null) {
                     names.remove(name);
                 }
                 if (out != null) {
                     writers.remove(out);
+                    updateClients();
                 }
                 try {
                     socket.close();
@@ -163,7 +148,7 @@ public class ChatServer {
                 }
             }
         }
-        
+
         //Check if a message is intended for a specific client
         public boolean isUnicastMessage(String message) {
             for (String clientName : names) {
@@ -174,7 +159,7 @@ public class ChatServer {
             }
             return false;
         }
-        
+
         //Extract only the message from a unicast message
         public String getUnicastRecipent(String message) {
             String unicastClient = null;
@@ -187,21 +172,28 @@ public class ChatServer {
             }
             return unicastClient;
         }
-        
+
         //Removes specified client from the list
-        public void removeClient(String clientName){
+        public void removeClient(String clientName) {
             writerMap.remove(clientName);
             System.out.println("Client Removed: " + clientName);
         }
-        
+
         //Returns a list of clients to the client side
-        public static String getAllClients(){
+        public static String getAllClients() {
             String[] keys = writerMap.keySet().toArray(new String[0]);
             String clientArray = "All";
             for (int i = 0; i < keys.length; i++) {
                 clientArray = clientArray + "," + keys[i];
             }
             return clientArray;
+        }
+
+        //Updates client list when a new client is added or removed
+        public void updateClients() {
+            for (PrintWriter writer : writers) {
+                writer.println("CLIENTLIST" + getAllClients());
+            }
         }
     }
 }
