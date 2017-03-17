56469b826b3bc9db005f635eb5297d9da4719d5e
Lanil Marasinghe <Lanil Marasinghe>
2017-03-17 21:05:19 +0530

    First commit

---
+++ test/ds_lab_1/ChatServerTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ds_lab_1;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Lanil Marasinghe
+ */
+public class ChatServerTest {
+    
+    public ChatServerTest() {
+    }
+
+    /**
+     * Test of main method, of class ChatServer.
+     */
+    @Test
+    public void testIsUnicastMessage(){
+        
+    }
+    
+}
